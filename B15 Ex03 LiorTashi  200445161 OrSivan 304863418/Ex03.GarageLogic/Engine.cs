1f2f75e52f33a1778cf948e22e61702b3c2ba2b1
Lior <leorz@me.com>
2015-05-22 18:22:21 +0300

    Added desired exception and engine class

---
+++ B15 Ex03 LiorTashi  200445161 OrSivan 304863418/Ex03.GarageLogic/Engine.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ex03.GarageLogic
+{
+    public class Engine
+    {
+        protected float m_CurrentQuantity;
+        protected float m_MaxCapacity;
+
+        protected void charge(float i_Quantity)
+        {
+            if (m_CurrentQuantity + i_Quantity > m_MaxCapacity)
+            {
+               throw new ValueOutOfRangeException(m_CurrentQuantity, m_MaxCapacity, i_Quantity);
+            }
+        }
+    }
+}
---
+++ B15 Ex03 LiorTashi  200445161 OrSivan 304863418/Ex03.GarageLogic/ValueOutOfRangeException.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ex03.GarageLogic
+{
+     public class ValueOutOfRangeException : Exception
+    {
+        private float m_CurrentQuantity;
+        private float m_MaxCapacity;
+        private float m_Quantity;
+
+         protected float CurrentQuantity
+        {
+             get { return m_CurrentQuantity; }
+        }
+
+        protected float MaxCapacity
+        {
+            get { return m_MaxCapacity; }
+        } 
+
+        protected float Quantity
+        {
+            get { return m_Quantity; }
+        }
+
+        public ValueOutOfRangeException (float i_CurrentQuantity, float i_MaxCapacity, float i_Quantity) 
+            // sending two params to the base CTOR:
+            : base(string.Format("Error, you are trying to fill the egine with {0} fuel when the current capacity of the engine is {1}, but the max capacity is {2}", i_Quantity, i_CurrentQuantity, i_MaxCapacity);
+        { }
+        }
+    }
+}
