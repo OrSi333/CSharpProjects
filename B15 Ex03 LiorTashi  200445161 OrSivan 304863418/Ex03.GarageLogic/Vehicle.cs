1fd228157925ec24fd4bdab4684b3b91f3befe1e
OrSi333 <orsivan22@gmail.com>
2015-05-22 14:15:19 +0300

    Added Bike.cs

---
+++ B15 Ex03 LiorTashi  200445161 OrSivan 304863418/Ex03.GarageLogic/Vehicle.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ex03.GarageLogic
+{
+    abstract internal class Vehicle
+    {
+        private string m_modelName;
+        private string m_LicenseNumber;
+        protected float m_EnergyLeft;
+        protected Wheel[] m_WheelSet;
+    }
+}
