3235093ea5a170718bfab6f15810ffdfd5884322
OrSi333 <orsivan22@gmail.com>
2015-05-22 10:32:18 +0300

    Added ex3

---
+++ B15 Ex03 LiorTashi  200445161 OrSivan 304863418/Ex03.GarageLogic/Wheel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ex03.GarageLogic
+{
+    internal class Wheel
+    {
+        private string m_makerName;
+        private float m_currentAirPressure;
+        private readonly float m_maxAirPressure;
+
+        internal Wheel(string i_MakerName, float i_MaxAirPressure)
+        {
+            m_makerName = i_MakerName;
+            m_maxAirPressure = i_MaxAirPressure;
+
+            //Inflate wheel to maximum on creation
+            m_currentAirPressure = m_maxAirPressure;
+        }
+
+        //TODO: Add the correct exception
+        internal void InflateWheel(float i_AirToAdd)
+        {
+            if (m_currentAirPressure + i_AirToAdd > m_maxAirPressure)
+            {
+                throw new Exception();
+            }
+            else
+            {
+                m_currentAirPressure += i_AirToAdd;
+            }
+        }
+    }
+}
